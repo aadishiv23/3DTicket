2779208e17b479ff64f61b3786bd8ed5d20abc12
aadishiv <aadishiv@outlook.com>
2025-01-10 18:31:30 -0500

    first commit

---
+++ 3DTicket/_DTicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DTicketApp.swift
+//  3DTicket
+//
+//  Created by Aadi Shiv Malhotra on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _DTicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
